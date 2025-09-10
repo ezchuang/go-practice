7c29d619cea943011aa10d215d6081dcdc43896e
ezchuang <t1375844@gmail.com>
2025-09-10 11:20:02 +0800

    update

--- main.go
+++ main.go
@@ -2,6 +2,33 @@ package main
 
 import "fmt"
 
+// 1
+// func main() {
+// 	fmt.Println("Hi there!")
+// }
+
+// 2
+// func main() {
+// 	var card string = "Five of Diamonds"
+
+// 	fmt.Println(card)
+// }
+
+// 3
+// func main() {
+// 	card := "Ace of Spades"
+// 	card = "Five of Diamonds"
+
+// 	fmt.Println(card)
+// }
+
+// 4
 func main() {
-	fmt.Println("Hi there!")
+	card := newCard()
+
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
